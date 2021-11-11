9d96fb584f3608b0ef0be45722efbd425442f4ed
Le Chen <le.chen@revas.io>
2021-11-11 18:17:19 +0100

    feat: add solutions

--- 04-colleghiamo-funzione-a-telegram/soluzione/api/bot.py
+++ 04-colleghiamo-funzione-a-telegram/soluzione/api/bot.py
@@ -1,10 +1,12 @@
 from http.server import BaseHTTPRequestHandler
+from telegram import Bot
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
+    bot = Bot(token='TOKEN_TELEGRAM_BOT')
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str("hello").encode())
+    self.wfile.write(str(bot.get_me()).encode())
     return
--- 05-registrazione-webhook/soluzione/api/bot.py
+++ 05-registrazione-webhook/soluzione/api/bot.py
@@ -1,10 +1,17 @@
 from http.server import BaseHTTPRequestHandler
+from telegram import Bot
+import json
+import urllib.request
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
+    token='TOKEN_TELEGRAM_BOT'
+    url = 'https://VERCEL_URL/api/bot/'
+    bot = Bot(token=token)
+    res = bot.setWebhook(url)
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    self.wfile.write(str("hello").encode())
+    self.wfile.write(str(res).encode())
     return
